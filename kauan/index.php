9a62a73c7f718b949ab3e50c945439d033a1556c
enthony17 <125404126+enthony17@users.noreply.github.com>
2023-03-22 15:20:50 -0300

    Update index.php

--- kauan/index.php
+++ kauan/index.php
@@ -29,7 +29,7 @@
     $result=$conn -> query($sql);
     if ($result->num_row >0 ) {
         while ($row=$result->fetch_assoc()){
-            // echo"nome:". $row ["nome"]."sobrenome :"$row ["sobrenome"]."telefone :"$row ["telefone"]."<br>";
+          echo"nome:". $row ["nome"]."sobrenome :"$row ["sobrenome"]."telefone :"$row ["telefone"]."<br>";
 
         }else {
             echo"table vazia";
